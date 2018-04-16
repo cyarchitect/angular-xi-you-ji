4f3d3c969c352dc21c65ca70d1dd72d9cfe6ee19
Yu Cheng <11217121+cyarchitect@users.noreply.github.com>
2018-04-15 22:20:18 -0400

    added master components

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,10 +1,10 @@
-import {Hero} from "./hero";
+import {Hero} from './hero';
 
 export const HEROES: Hero[] = [
   { id: 1, name: '孙悟空' },
   { id: 2, name: '猪八戒' },
   { id: 3, name: '沙和尚' },
-  { id: 4, name: '白龙马' }，
-  { id: 5, name: 'Leo' }，
+  { id: 4, name: '白龙马' },
+  { id: 5, name: 'Leo' },
   { id: 6, name: 'Mark' }
 ];
